97096acfac0714e8d4e3ceac58d38a199a164782
Flavio Silva <55815265+201flaviosilva@users.noreply.github.com>
2022-02-01 14:36:51 +0000

    First version 1.0.0

--- src/Guardar.js
+++ src/Guardar.js
@@ -1,6 +1,64 @@
-export function print(text) { console.log(text); }
+export let saveDataName = "guardar";
+
+export function init(newSaveDataName = saveDataName) {
+	saveDataName = newSaveDataName;
+	localStorage.getItem(saveDataName) || localStorage.setItem(saveDataName, "{}");
+}
+
+export function getAll() {
+	return JSON.parse(localStorage.getItem(saveDataName));
+}
+
+export function getData(name) {
+	let data = getAll();
+	return data[name];
+}
+
+export function setData(name, value) {
+	let saveData = getAll();
+	saveData[name] = value;
+	updateAll(saveData);
+}
+
+export function updateAll(data) {
+	localStorage.setItem(saveDataName, JSON.stringify(data));
+}
+
+export function removeData(name) {
+	let saveData = getAll();
+	delete saveData[name];
+	updateAll(saveData);
+}
+
+export function clear() {
+	// localStorage.removeItem(saveDataName);
+	updateAll({});
+}
+
+export function changeSaveDataName(name) {
+	let saveData = getAll();
+	localStorage.removeItem(saveDataName);
+	init(name);
+	updateAll(saveData);
+}
+
+export function keys() { return Object.keys(getAll()); }
+export function size() { return keys().length; }
+export function has(name) { return keys().includes(name); }
+export function print() { console.log(getAll()); }
 
 const Guardar = {
+	init,
+	getAll,
+	getData,
+	setData,
+	updateAll,
+	removeData,
+	clear,
+	changeSaveDataName,
+	keys,
+	size,
+	has,
 	print,
 };
 
