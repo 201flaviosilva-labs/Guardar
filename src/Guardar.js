990aa066de29d47d6b742975e70c245cea521bf4
Flavio Silva <55815265+201flaviosilva@users.noreply.github.com>
2022-02-02 18:37:50 +0000

    doc: Documenting Code

--- src/Guardar.js
+++ src/Guardar.js
@@ -1,51 +1,115 @@
+/**
+ * @type {string} saveDataName
+ */
 export let saveDataName = "guardar";
 
+/**
+ * Start the local storage
+ * 
+ * @param {string} newSaveDataName the name to save the data in the local storage
+ */
 export function init(newSaveDataName = saveDataName) {
 	saveDataName = newSaveDataName;
 	localStorage.getItem(saveDataName) || localStorage.setItem(saveDataName, "{}");
 }
 
+/**
+ * Get all data saved in the local storage
+ * 
+ * @returns {object} the data saved in the local storage
+ */
 export function getAll() {
 	return JSON.parse(localStorage.getItem(saveDataName));
 }
 
+/**
+ * Get a specific data saved in the local storage
+ * 
+ * @param {string} name key of the data to get
+ * @returns {any} the data saved in the local storage
+ */
 export function getData(name) {
 	let data = getAll();
 	return data[name];
 }
 
+/**
+ * Save/change a specific data by name
+ * 
+ * @param {string} name name of the key
+ * @param {any} value value of the item
+ */
 export function setData(name, value) {
 	let saveData = getAll();
 	saveData[name] = value;
 	updateAll(saveData);
 }
 
+/**
+ * Save/change all data
+ * 
+ * @param {any} data data to save
+ */
 export function updateAll(data) {
 	localStorage.setItem(saveDataName, JSON.stringify(data));
 }
 
+/**
+ * Delete a specific data by name
+ * 
+ * @param {sting} name the name of the data to delete
+ */
 export function removeData(name) {
 	let saveData = getAll();
 	delete saveData[name];
 	updateAll(saveData);
 }
 
-export function clear() {
-	// localStorage.removeItem(saveDataName);
-	updateAll({});
-}
-
-export function changeSaveDataName(name) {
+/**
+ * Change the name of the local storage
+ * 
+ * @param {sting} name new name of the local storage
+ */
+export function changeSaveDataName(name = saveDataName) {
 	let saveData = getAll();
 	localStorage.removeItem(saveDataName);
 	init(name);
 	updateAll(saveData);
 }
 
+/**
+ * Delete all data saved in the local storage
+ */
+export function clear() { updateAll({}); }
+
+/**
+ * Get keys of the stored items
+ * 
+ * @returns {string[]} return all keys
+ */
 export function keys() { return Object.keys(getAll()); }
+
+/**
+ * Get number of stored items
+ * 
+ * @returns {number} number of stored items
+ */
 export function size() { return keys().length; }
-export function has(name) { return keys().includes(name); }
-export function print() { console.log(getAll()); }
+
+/**
+ * Check if the given key is stored
+ * 
+ * @param {string} key key to check
+ * @returns {boolean} true if the key is stored
+ */
+export function has(key) { return keys().includes(key); }
+
+/**
+ * Check if the local storage is empty
+ * 
+ * @returns {boolean} true if the local storage is empty
+ */
+export function isEmpty() { return size() === 0; }
 
 const Guardar = {
 	init,
