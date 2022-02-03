4207a516a979e89aef4be24206caf8d29dd348cc
Flavio Silva <55815265+201flaviosilva@users.noreply.github.com>
2022-02-03 12:27:03 +0000

    GitHub Pages

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,13 @@ import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import babel from "rollup-plugin-babel";
 
+const buildFolder = "dist/lib/"; // used for deploy in github pages
+
 const umd = {
 	input: "src/Guardar.js",
 	output: {
 		name: "guardar",
-		file: "dist/Guardar.umd.js", // Universal Module Definition
+		file: buildFolder + "Guardar.umd.js", // Universal Module Definition
 		format: "umd",
 	},
 	plugins: [
@@ -19,15 +21,15 @@ const umd = {
 };
 
 export default [
-	// umd,
+	umd,
 	{
 		input: "src/Guardar.js",
 		output: [ // https://betterprogramming.pub/what-are-cjs-amd-umd-esm-system-and-iife-3633a112db62
-			{ file: "dist/Guardar.esm.js", format: "es" }, // ES Module
-			{ file: "dist/Guardar.cjs.js", format: "cjs" }, // CommonJS (Node)
-			// { file: "dist/Guardar.amd.js", format: "amd" }, // Asynchronous Module Definition
-			// { file: "dist/Guardar.system.js", format: "system" }, // 
-			// { file: "dist/Guardar.iife.js", format: "iife" }, // IIFE (vanilla) probably useless
+			{ file: buildFolder + "Guardar.esm.js", format: "es" }, // ES Module
+			{ file: buildFolder + "Guardar.cjs.js", format: "cjs" }, // CommonJS (Node)
+			{ file: buildFolder + "Guardar.amd.js", format: "amd" }, // Asynchronous Module Definition
+			{ file: buildFolder + "Guardar.system.js", format: "system" }, // 
+			{ file: buildFolder + "Guardar.iife.js", format: "iife" }, // IIFE (vanilla) probably useless
 		],
 		plugins: [
 			babel({
