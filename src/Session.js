917ef65d5a28c8988bf60e2a67eaf5dd77f310d5
Flavio Silva <55815265+201flaviosilva@users.noreply.github.com>
2022-06-23 18:50:09 +0100

    feat: Session Storage #3

---
+++ src/Session.js
@@ -0,0 +1,177 @@
+/**
+ * @class Session
+ * @classdesc Class for dealing with session storage.
+ * 
+ * @example Session.getAll();
+ */
+export default class Session {
+	constructor() {
+		this.name = "guardar";
+		this._init();
+	}
+
+	/**
+	 * Core function to initialize the session storage api
+	 * 
+	 * @returns {void}
+	 * @memberof Session
+	 * @private
+	 */
+	_init() {
+		sessionStorage.getItem(this.name) || sessionStorage.setItem(this.name, "{}");
+	}
+
+	/**
+	 * Change the name of data saved in the Session storage
+	 * 
+	 * @example Session._init("mySessionStorage");
+	 * 
+	 * @param {string} newSaveDataName - the name to save the data in the Session storage
+	 * @returns {void}
+	 * @memberof Session
+	 */
+	setName(newSaveDataName = this.name) {
+		this.name = newSaveDataName;
+		this._init();
+	}
+
+	/**
+	 * Get all data saved in the Session storage
+	 * 
+	 * @example Session.getAll();
+	 * 
+	 * @returns {object} the data saved in the Session storage
+	 * @memberof Session
+	 */
+	getAll() {
+		return JSON.parse(sessionStorage.getItem(this.name));
+	}
+
+	/**
+	 * Get a specific data saved in the Session storage
+	 * 
+	 * @example Session.getData("myKey");
+	 * 
+	 * @param {string} key - key of the data to get
+	 * @returns {any} the data saved in the Session storage
+	 * @memberof Session
+	 */
+	getData(key) {
+		let data = this.getAll();
+		return data[key];
+	}
+
+	/**
+	 * Save/change a specific data by key
+	 * 
+	 * @example Session.setData("myKeyString", "myString");
+	 * @example Session.setData("myKeyBool", true);
+	 * @example Session.setData("myKeyNumber", 1234567890);
+	 * @example Session.setData("myKeyObject", {name: "Dog", age: 3,});
+	 * @example Session.setData("myKeyArr", [1, 2, 3, 4, 5]);
+	 * 
+	 * @param {string} key - name of the key
+	 * @param {any} value value of the item
+	 * @returns {void}
+	 * @memberof Session
+	 */
+	setData(key, value) {
+		let saveData = this.getAll();
+		saveData[key] = value;
+		this.updateAll(saveData);
+	}
+
+	/**
+	 * Save/change all data
+	 * 
+	 * @example Session.updateAll({ otherNewData: "Hello World" });
+	 * 
+	 * @param {any} data - data to save
+	 * @returns {void}
+	 * @memberof Session
+	 */
+	updateAll(data) {
+		sessionStorage.setItem(this.name, JSON.stringify(data));
+	}
+
+	/**
+	 * Delete a specific data by name
+	 * 
+	 * @example Session.removeData("myKey");
+	 * 
+	 * @param {sting} key - the name of the data to delete
+	 * @returns {void}
+	 * @memberof Session
+	 */
+	removeData(key) {
+		let saveData = this.getAll();
+		delete saveData[key];
+		this.updateAll(saveData);
+	}
+
+	/**
+	 * Change the name of the Session storage
+	 * 
+	 * @example Session.changeSaveDataName("sessionStorageName");
+	 * 
+	 * @param {sting} name - new name of the Session storage
+	 * @memberof Session
+	 */
+	changeSaveDataName(name = this.name) {
+		let saveData = this.getAll();
+		sessionStorage.removeItem(this.name);
+		this.setName(name);
+		this.updateAll(saveData);
+	}
+
+	/**
+	 * Delete all data saved in the Session storage
+	 * 
+	 * @example Session.clear();
+	 * 
+	 * @returns {void}
+	 * @memberof Session
+	 */
+	clear() { this.updateAll({}); }
+
+	/**
+	 * Get keys of the stored items
+	 * 
+	 * @example Session.keys();
+	 * 
+	 * @returns {string[]} return all keys
+	 * @memberof Session
+	 */
+	keys() { return Object.keys(this.getAll()); }
+
+	/**
+	 * Get number of stored items
+	 * 
+	 * @example Session.size();
+	 * 
+	 * @returns {number} number of stored items
+	 * @memberof Session
+	 */
+	size() { return this.keys().length; }
+
+	/**
+	 * Check if the given key is stored
+	 * 
+	 * @example Session.has("myKey");
+	 * 
+	 * @param {string} key - key to check
+	 * @returns {boolean} true if the key is stored
+	 * @memberof Session
+	 */
+	has(key) { return this.keys().includes(key); }
+
+	/**
+	 * Check if the Session storage is empty
+	 * 
+	 * @example Session.isEmpty();
+	 * 
+	 * @returns {boolean} true if the Session storage is empty
+	 * @memberof Session
+	 */
+	isEmpty() { return this.size() === 0; }
+}
---
+++ types/Session.d.ts
@@ -0,0 +1,136 @@
+/**
+ * @class Session
+ * @classdesc Class for dealing with session storage.
+ *
+ * @example Session.getAll();
+ */
+export default class Session {
+    name: string;
+    /**
+     * Core function to initialize the session storage api
+     *
+     * @returns {void}
+     * @memberof Session
+     * @private
+     */
+    private _init;
+    /**
+     * Change the name of data saved in the Session storage
+     *
+     * @example Session._init("mySessionStorage");
+     *
+     * @param {string} newSaveDataName - the name to save the data in the Session storage
+     * @returns {void}
+     * @memberof Session
+     */
+    setName(newSaveDataName?: string): void;
+    /**
+     * Get all data saved in the Session storage
+     *
+     * @example Session.getAll();
+     *
+     * @returns {object} the data saved in the Session storage
+     * @memberof Session
+     */
+    getAll(): object;
+    /**
+     * Get a specific data saved in the Session storage
+     *
+     * @example Session.getData("myKey");
+     *
+     * @param {string} key - key of the data to get
+     * @returns {any} the data saved in the Session storage
+     * @memberof Session
+     */
+    getData(key: string): any;
+    /**
+     * Save/change a specific data by key
+     *
+     * @example Session.setData("myKeyString", "myString");
+     * @example Session.setData("myKeyBool", true);
+     * @example Session.setData("myKeyNumber", 1234567890);
+     * @example Session.setData("myKeyObject", {name: "Dog", age: 3,});
+     * @example Session.setData("myKeyArr", [1, 2, 3, 4, 5]);
+     *
+     * @param {string} key - name of the key
+     * @param {any} value value of the item
+     * @returns {void}
+     * @memberof Session
+     */
+    setData(key: string, value: any): void;
+    /**
+     * Save/change all data
+     *
+     * @example Session.updateAll({ otherNewData: "Hello World" });
+     *
+     * @param {any} data - data to save
+     * @returns {void}
+     * @memberof Session
+     */
+    updateAll(data: any): void;
+    /**
+     * Delete a specific data by name
+     *
+     * @example Session.removeData("myKey");
+     *
+     * @param {sting} key - the name of the data to delete
+     * @returns {void}
+     * @memberof Session
+     */
+    removeData(key: sting): void;
+    /**
+     * Change the name of the Session storage
+     *
+     * @example Session.changeSaveDataName("sessionStorageName");
+     *
+     * @param {sting} name - new name of the Session storage
+     * @memberof Session
+     */
+    changeSaveDataName(name?: sting): void;
+    /**
+     * Delete all data saved in the Session storage
+     *
+     * @example Session.clear();
+     *
+     * @returns {void}
+     * @memberof Session
+     */
+    clear(): void;
+    /**
+     * Get keys of the stored items
+     *
+     * @example Session.keys();
+     *
+     * @returns {string[]} return all keys
+     * @memberof Session
+     */
+    keys(): string[];
+    /**
+     * Get number of stored items
+     *
+     * @example Session.size();
+     *
+     * @returns {number} number of stored items
+     * @memberof Session
+     */
+    size(): number;
+    /**
+     * Check if the given key is stored
+     *
+     * @example Session.has("myKey");
+     *
+     * @param {string} key - key to check
+     * @returns {boolean} true if the key is stored
+     * @memberof Session
+     */
+    has(key: string): boolean;
+    /**
+     * Check if the Session storage is empty
+     *
+     * @example Session.isEmpty();
+     *
+     * @returns {boolean} true if the Session storage is empty
+     * @memberof Session
+     */
+    isEmpty(): boolean;
+}
